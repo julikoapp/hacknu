6063232ecef033d76ae9f66b6328fedd695aed3d
Juliko <Ulia@mAir.local>
2023-04-15 15:22:15 +0600

    added Login and Register Form

--- epam_project/settings.py
+++ epam_project/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+PROJECT_DIR = os.path.dirname(os.path.dirname(__file__))
+TEMPLATE_DIRS = (os.path.join(PROJECT_DIR, 'templates'), )
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'epam_project',
+    
 ]
 
 MIDDLEWARE = [
